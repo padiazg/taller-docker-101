ff517ca58c3046a1a7500137cac340b2eb09fc8e
Patricio Diaz <padiazg@gmail.com>
2019-07-05 10:21:26 -0400

    first commit

---
+++ ejemplo-01/app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(3000, () => console.log("Ejemplo escuchando en el puerto 3000!"));
